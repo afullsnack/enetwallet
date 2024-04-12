f69f895037c82f7a7796ca326cad2bc01cf0f3df
Miracle Friday <miraclef60@gmail.com>
2024-04-12 19:39:41 +0100

    fix: fixed login confirm email and main screens

--- src/app/(auth)/(login)/main.tsx
+++ src/app/(auth)/(login)/main.tsx
@@ -1,5 +1,6 @@
+import { Container } from "@/components/Container";
 import { Button } from "@/components/button";
-import Input from "@/components/input";
+import { Input } from "@/components/input";
 import { Image } from "expo-image";
 import { Link, Stack } from "expo-router";
 import { useState } from "react";
@@ -23,82 +24,108 @@ export default function LoginIndexPage() {
   const [password, setPassword] = useState("");
 
   return (
-    <SafeAreaView>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "android" ? "height" : "padding"}
-      >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View className="w-ful container min-h-screen bg-[#0C0C12] px-6 md:px-8">
-            <View className="flex flex-col items-center gap-2">
-              <View className="my-5 flex flex-row items-center justify-center">
-                <View className="mr-1 h-1 w-1 rounded-full bg-white" />
-                <Text className="text-xl font-medium leading-snug tracking-normal text-white">
-                  EnetWallet{" "}
-                </Text>
-                <View className="h-1 w-1 rounded-full bg-white" />
-              </View>
-              <View className="my-4">
+    <Container>
+      <View className="w-full min-h-screen px-6">
+        <View className="flex flex-col items-center">
+          <View className="my-5 flex flex-row items-center justify-center">
+            <View className="mr-1 h-1 w-1 rounded-full bg-white" />
+            <Text className="text-xl font-medium leading-snug tracking-normal text-white">
+              EnetWallet{" "}
+            </Text>
+            <View className="h-1 w-1 rounded-full bg-white" />
+          </View>
+          <View className="my-4">
+            <Image
+              source={require("../../../../assets/onboarding-1.png")}
+              style={{ width: 210, height: 226 }}
+              className="bg-contain"
+            />
+          </View>
+          <Input
+            outline={true}
+            value={identifier}
+            placeholder="Phone or Email Address"
+            placeholderTextColor="#18EAFF"
+            onChangeText={setIdentifier}
+            keyboardType="default"
+            containerStyle={{
+              marginTop: 20,
+              borderRadius: 10,
+            }}
+            style={{
+              height: 52,
+              borderRadius: 10,
+              color: "#18EAFF",
+              opacity: 0.5,
+            }}
+          />
+          <Input
+            outline={true}
+            value={password}
+            placeholder="Password"
+            placeholderTextColor="#18EAFF"
+            onChangeText={setPassword}
+            keyboardType="default"
+            textContentType="password"
+            secureTextEntry
+            containerStyle={{
+              borderRadius: 10,
+              marginTop: 20,
+            }}
+            style={{
+              height: 52,
+              borderRadius: 10,
+              color: "#18EAFF",
+              opacity: 0.5,
+            }}
+          />
+          <Button
+            onPress={() => {
+              console.log(identifier, password, ":::PAssword");
+            }}
+            style={{
+              width: "100%",
+              borderRadius: 17,
+              marginTop: 20,
+            }}
+          >
+            <Text>Login</Text>
+          </Button>
+          <Link
+            href="/recovery/email"
+            style={{
+              marginTop: 30,
+            }}
+          >
+            <Text className="text-[#18EAFF]">Forgot password?</Text>
+          </Link>
+
+          <View className="mt-4 grid gap-8">
+            <View className="flex flex-row items-center justify-center gap-4">
+              <View className="h-0.5 w-full bg-white/60" />
+              <Text className="text-white">Get started</Text>
+              <View className="h-0.5 w-full bg-white/60" />
+            </View>
+            <View className="flex flex-row items-center justify-center gap-4">
+              <TouchableOpacity className="rounded-2xl border border-[#18EAFF] p-2">
                 <Image
-                  source={require("../../../../assets/onboarding-1.png")}
-                  style={{ width: 210, height: 226 }}
+                  source={require("../../../../assets/socials-1.png")}
+                  style={{ width: 20, height: 20 }}
                   className="bg-contain"
                 />
-              </View>
-              <Input
-                props={{
-                  value: identifier,
-                  placeholder: "Phone or Email Address",
-                  onChangeText: setIdentifier,
-                  keyboardType: "default",
-                }}
-              />
-              <Input
-                props={{
-                  value: password,
-                  placeholder: "Password",
-                  onChangeText: setPassword,
-                  keyboardType: "default",
-                }}
-              />
-              <Button
-                onPress={() => {
-                  console.log(identifier, password, ":::PAssword");
-                }}
-              >
-                <Text>Login</Text>
-              </Button>
-              <Link href="/recovery/" className="mt-5">
-                <Text className="text-[#18EAFF]">Forgot password?</Text>
-              </Link>
-
-              <View className="mt-4 grid gap-8">
-                <View className="flex flex-row items-center justify-center gap-4">
-                  <View className="h-0.5 w-full bg-white/60" />
-                  <Text className="text-white">Get started</Text>
-                  <View className="h-0.5 w-full bg-white/60" />
-                </View>
-                <View className="flex flex-row items-center justify-center gap-4">
-                  <TouchableOpacity className="rounded-2xl border border-[#18EAFF] p-2">
-                    <Image
-                      source={require("../../../../assets/socials-1.png")}
-                      style={{ width: 20, height: 20 }}
-                      className="bg-contain"
-                    />
-                  </TouchableOpacity>
-                  <View className="h-full w-0.5 bg-white/60" />
-                  <TouchableOpacity className="rounded-2xl border border-[#18EAFF] p-2">
-                    <Image
-                      source={require("../../../../assets/socials-2.png")}
-                      style={{ width: 20, height: 20 }}
-                      className="bg-contain"
-                    />
-                  </TouchableOpacity>
-                </View>
-              </View>
+              </TouchableOpacity>
+              <View className="h-full w-0.5 bg-white/60" />
+              <TouchableOpacity className="rounded-2xl border border-[#18EAFF] p-2">
+                <Image
+                  source={require("../../../../assets/socials-2.png")}
+                  style={{ width: 20, height: 20 }}
+                  className="bg-contain"
+                />
+              </TouchableOpacity>
             </View>
           </View>
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+        </View>
+      </View>
+    </Container>
   );
 }

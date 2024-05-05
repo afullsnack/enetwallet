debe903115779edc4d6fdc52d4201a66a7afc93c
Miracle Friday <miraclef60@gmail.com>
2024-05-05 09:43:25 +0100

    feat: added custom tab view for swap screen

--- src/components/tab_view.tsx
+++ src/components/tab_view.tsx
@@ -124,4 +124,84 @@ const SubTab: React.FC<{
   );
 };
 
-export { SubTab, TabView };
+const SwapTab: React.FC<{
+  items: React.ReactNode[];
+  tabList: string[];
+  extra?: React.ReactNode;
+}> = ({ items, tabList, extra }) => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  return (
+    <View className="grid w-full items-center justify-start">
+      <View
+        className="mb-2 flex w-full items-center"
+        style={{
+          paddingHorizontal: 20,
+          // marginHorizontal: 20,
+          // marginVertical: "auto",
+        }}
+      >
+        <View
+          style={{
+            paddingVertical: 6,
+            flexDirection: "row",
+            backgroundColor: "#12131B",
+            width: "100%",
+            borderRadius: 11,
+            height: items ? "auto" : 42,
+            alignContent: items ? "space-between" : "center",
+            justifyContent: "center",
+          }}
+        >
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            data={tabList}
+            renderItem={({ item, index }) => (
+              <TouchableOpacity
+                className="flex items-center justify-center px-4 py-1"
+                style={{
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gap: 4,
+                  // borderBottomWidth: 1,
+                  // borderBottomColor:
+                  //   activeIndex === index ? "#18EAFF" : "transparent",
+                }}
+                onPress={() => {
+                  console.log(index, "Index");
+                  setActiveIndex(index);
+                }}
+              >
+                <Text
+                  style={{
+                    color: activeIndex === index ? "#18EAFF" : "#49515D",
+                  }}
+                >
+                  {item}
+                </Text>
+                <View
+                  style={{
+                    height: 2.2,
+                    width: "75%",
+                    borderRadius: 9999,
+                    backgroundColor:
+                      activeIndex === index ? "#18EAFF" : "transparent",
+                  }}
+                />
+              </TouchableOpacity>
+            )}
+            horizontal
+            style={{
+              width: items ? "auto" : "100%",
+            }}
+          />
+          {extra}
+        </View>
+      </View>
+      <View className="w-full mt-5">{items[activeIndex]}</View>
+    </View>
+  );
+};
+
+export { SubTab, TabView, SwapTab };
